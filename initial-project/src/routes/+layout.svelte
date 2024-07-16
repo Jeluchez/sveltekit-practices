73781895c8abd3fbd36dc4b9bc35b4c749975a5f
jose luis <joseluis.sanchez@globant.com>
2024-07-16 16:36:09 -0500

    default action

--- initial-project/src/routes/+layout.svelte
+++ initial-project/src/routes/+layout.svelte
@@ -1,17 +1,30 @@
 <script>
-	import Header from './Header.svelte';
-	import '../app.css';
+	import Header from "./Header.svelte";
+	import "../app.css";
 </script>
 
 <div class="app">
 	<Header />
-
+	<form method="POST" action="/login">
+		<p>the main form</p>
+		<label>
+			Email
+			<input name="email" type="email" />
+		</label>
+		<label>
+			Password
+			<input name="password" type="password" />
+		</label>
+		<button>Log in</button>
+	</form>
 	<main>
 		<slot />
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>
+			visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+		</p>
 	</footer>
 </div>
 
---
+++ initial-project/src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<form method="POST">
+    <label>
+        Email
+        <input name="email" type="email" />
+    </label>
+    <label>
+        Password
+        <input name="password" type="password" />
+    </label>
+    <button>Log in</button>
+</form>
+<form method="POST" action="?/register">
+    <label>
+        Email
+        <input name="email" type="email" />
+    </label>
+    <label>
+        Password
+        <input name="password" type="password" />
+    </label>
+    <button>Log in</button>
+</form>
